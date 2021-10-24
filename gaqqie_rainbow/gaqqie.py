307086a89d91aaf911f3094415ca2447f3c190bd
Satoyuki Tsukano <tknstyk@gmail.com>
2021-10-24 23:27:35 +0900

    refactoring

--- gaqqie_rainbow/gaqqie.py
+++ gaqqie_rainbow/gaqqie.py
@@ -32,7 +32,24 @@ class EventThread(Thread):
 
 
 class Gaqqie:
+    """The Gaqqie object implements a job execution application.
+
+    This repeats the following process:
+
+    - Pulls new jobs at user-specified intervals.
+    - When this pulls a new job, executes the function specified by the user on a worker thread.
+
+    This creates a worker thread for each function specified by the user.
+    """
+
     def __init__(self, url: str) -> None:
+        """Initializes Gaqqie object.
+
+        Parameters
+        ----------
+        url : str
+            the base URL of the gaqqie API for providers.
+        """
         signal.signal(signal.SIGINT, signal.SIG_DFL)
         self._event_list = []
         self._thread_list = []
@@ -47,24 +64,65 @@ class Gaqqie:
 
     @property
     def job_api(self) -> JobApi:
+        """Returns "job interface" of the gaqqie API for providers.
+
+        Returns
+        -------
+        JobApi
+            job interface.
+        """
         return self._job_api
 
     @property
     def device_api(self) -> DeviceApi:
+        """Returns "device interface" of the gaqqie API for providers.
+
+        Returns
+        -------
+        DeviceApi
+            device interface.
+        """
         return self._device_api
 
     @property
     def provider_api(self) -> ProviderApi:
+        """Returns "provider interface" of the gaqqie API for providers.
+
+        Returns
+        -------
+        ProviderApi
+            provider interface.
+        """
         return self._provider_api
 
     def join(self) -> None:
+        """Blocks the thread that called this function until all worker threads are stopped."""
         [thread.join() for thread in self._thread_list]
 
     def stop(self) -> None:
+        """Stops all worker threads."""
         for event in self._event_list:
             event.clear()
 
     def receive_job(self, device_name: str, interval: float = 10.0) -> Callable:
+        """Create and run worker thread.
+
+        This function is supposed to be used as a decorator.
+        Decorates the function that executes jobs.
+
+        Parameters
+        ----------
+        device_name : str
+            a specific device name on which to run the job.
+        interval : float, optional
+            polling interval(seconds) to the gaqqie API for providers, by default 10.0
+
+        Returns
+        -------
+        Callable
+            the decorator function.
+        """
+
         def decorator(func: Callable) -> Callable:
             @wraps(func)
             def wrapper(*args, **kwargs) -> None:
@@ -84,4 +142,14 @@ class Gaqqie:
         return decorator
 
     def register_result(self, result: Result) -> None:
+        """Registers the results of job execution via the gaqqie API for providers.
+
+        It is assumed that the provider uses the result of the job execution to execute this function.
+        The provider should execute this function even if the job fails to run.
+
+        Parameters
+        ----------
+        result : Result
+            the result of the job execution.
+        """
         self.job_api.register_result(result, result.job_id)
